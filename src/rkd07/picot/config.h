0251272b608c1b8386449da8ffef82f050b11091
rikkodo <ctrlalt.exit@gmail.com>
2025-05-21 21:47:47 +0900

    RKD07 picot firm first commit

---
+++ src/rkd07/picot/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define PMW33XX_CS_PIN GP5
+#define SPI_SCK_PIN GP2
+#define SPI_MISO_PIN GP4
+#define SPI_MOSI_PIN GP3
+
+#define I2C1_SCL_PIN GP19
+#define I2C1_SDA_PIN GP18
+#define I2C_DRIVER I2CD1
