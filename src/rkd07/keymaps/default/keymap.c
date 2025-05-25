199bf93a77ef6d78ec082884311163919cacde90
rikkodo <ctrlalt.exit@gmail.com>
2025-05-25 16:37:17 +0900

    Enable Encoder

--- src/rkd07/keymaps/default/keymap.c
+++ src/rkd07/keymaps/default/keymap.c
@@ -14,3 +14,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
     // clang-format on
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    // clang-format off
+    [0] = {
+        ENCODER_CCW_CW(KC_BSPC, KC_TAB),
+    },
+    // clang-format on
+};
+#endif
